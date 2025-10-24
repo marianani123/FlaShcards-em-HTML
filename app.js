c4c58d8c9e8db300af1ea5b99bda81009a322a2a
marianani123 <daniele.oliveira.garcia@escola.pr.gov.br>
2025-10-24 10:45:23 -0300

    .

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ function criCartao(categoria,pergunta,resposta){
     let cartao=document.createElement('article')
     cartao.className='cartao'
     
-}
+}cartao.inneHTML=`
+<dix class=">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo_pergunta">
+<p>${pergunta}</p>
+
+</div>
+<div class=cartao_conteudop_resposta">
+<p>${resposta}</p>
+<div>
+</div>
---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de progração'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+criaCartao(
+    'Progração',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em ingles',
+    'Oi em ingles é HI (RAI)'
+)
