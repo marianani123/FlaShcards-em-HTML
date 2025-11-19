33c6749509ffc8bd8747eda855b11439e5ae2326
marianani123 <daniele.oliveira.garcia@escola.pr.gov.br>
2025-11-19 11:12:55 -0300

    ...............

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container');
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-    // Criar elemento do cartão
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
-
-    // Conteúdo interno do cartão
     cartao.innerHTML = `
-        <div class="cartao__conteudo">
-            <h3>${categoria}</h3>
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
 
-            <div class="cartao__conteudo__pergunta">
-                <p>${pergunta}</p>
-            </div>
-
-            <div class="cartao__conteudo__resposta">
-                <p>${resposta}</p>
-            </div>
-        </div>
-    `;
-
-    // Controle de visibilidade da resposta
-    let respostaEstaVisivel = false;
+    let respostaEstaVisivel = false
 
     function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active', respostaEstaVisivel);
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
+    cartao.addEventListener('click', viraCartao)
 
-    // Evento de clique
-    cartao.addEventListener('click', viraCartao);
 
-    // Adicionando ao container
-    container.appendChild(cartao);
+    container.appendChild(cartao)
+
 }
--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,24 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de progração'
+    'O Python é uma linguagem de programação'
 )
+
 criaCartao(
     'Geografia',
     'Qual a capital da França?',
-    'A capital da França é Paris',
+    'A capital da França é Paris'
 )
+
 criaCartao(
-    'Progração',
-    'O que é uma função',
+    'Programação',
+    'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em ingles',
+    'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
